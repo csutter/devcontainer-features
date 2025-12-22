e5dc7ba3eaa29cc261ca60c35f1f8c13bf0ed07a
Christian Sutter <git@doublesignal.com>
2025-12-22 14:36:08 +0000

    Add packages to `personal-setup` (#6)
    This adds `tree` and `fish` to the packages installed into
    devcontainers.

--- test/personal-setup/test.sh
+++ test/personal-setup/test.sh
@@ -3,7 +3,9 @@ set -e
 
 source dev-container-features-test-lib
 
+check "fish is available"       bash -c "which fish"
 check "rcm is available"        bash -c "which rcup"
+check "tree is available"       bash -c "which tree"
 # Use a dotfile that should _always_ be present on any system
 check "dotfiles are installed"  bash -c "test -f $HOME/.config/fish/config.fish"
 
